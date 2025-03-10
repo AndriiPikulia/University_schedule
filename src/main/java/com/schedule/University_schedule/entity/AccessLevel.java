096611277d296eac73ddd3f998f47e54286b9729
Andrii Pikulia <andrewpikulia@gmail.com>
2025-03-11 00:00:03 +0200

    Added Enum "AccessLevel" and added @Column annotations to @Entity

---
+++ src/main/java/com/schedule/University_schedule/entity/AccessLevel.java
@@ -0,0 +1,5 @@
+package com.schedule.University_schedule.entity;
+
+public enum AccessLevel {
+    CHIEF_ADMIN, ADMIN, HEAD_OF_THE_GROUP, STUDENT, UNAUTHORIZED;
+}
--- src/main/java/com/schedule/University_schedule/entity/Account.java
+++ src/main/java/com/schedule/University_schedule/entity/Account.java
@@ -4,24 +4,35 @@ import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.Setter;
 
+import javax.management.relation.Relation;
+
 @Setter
 @Getter
 @Entity
+@Table(name="Account")
 public class Account {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name="ID", nullable=false, updatable=false)
     private Long id;
 
+    @Column(name = "First name", nullable=false)
     private String firstName;
 
+    @Column(name = "Last name", nullable=false)
     private String lastName;
 
+    @Column(name = "Middle name", nullable=false)
     private String middleName;
 
+    @Column(name = "Email", nullable=false)
     private String email;
 
-    private String department;
+    @Column(name = "Department", nullable=false)
+    private Relation department;
 
-    private String accessLevel;
+    @Column(name = "Access level", nullable=false)
+    @Enumerated(EnumType.STRING)
+    private AccessLevel accessLevel;
 
 }
